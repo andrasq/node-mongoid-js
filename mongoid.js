be4175760134abf3491288157d959c06a046c26d
Andras <andras@kinvey.com>
2016-10-25 02:08:10 -0400

    update header

--- mongoid.js
+++ mongoid.js
@@ -3,7 +3,7 @@
  * Ids are a hex number built out of the timestamp, a per-server unique id,
  * the process id and a sequence number.
  *
- * Copyright (C) 2014 Andras Radics
+ * Copyright (C) 2014,2016 Andras Radics
  * Licensed under the Apache License, Version 2.0
  *
  * MongoDB object ids are 12 bytes (24 hexadecimal chars), composed out of
